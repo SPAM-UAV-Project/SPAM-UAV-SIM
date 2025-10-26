a75a9c83a8f100c5f42b7765f5fbc690fd980bcd
Tommy Zhang <tommyz2005@gmail.com>
2025-10-25 23:03:14 -0400

    initial commit

---
+++ params.m
@@ -0,0 +1,8 @@
+% scalar constants
+mass = 0.6; % kg
+
+% define body frame as FRD front right down
+% define inertial frame as NED north east down
+
+% moment arm of motors
+arm = [0, 0, 0.2; 0, 0, -0.2]';
