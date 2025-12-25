0da2642669a5564d7c1302aa24529bd2171ba3ac
Tommy Zhang <tommyz2005@gmail.com>
2025-12-25 01:15:52 -0500

    update controller allocation and actuator dynamics to prepare for sysid

--- params.m
+++ params.m
@@ -4,5 +4,12 @@ mass = 0.6; % kg
 % define body frame as FRD front right down
 % define inertial frame as NED north east down
 
-% moment arm of motors
-arm = [0, 0, 0.2; 0, 0, -0.2]';
+% propeller vars
+A_cut_in = 0.15; % modulation
+K_coeff = 0.2; % Nm / % modulation
+phase_lag = 0; % rad    (0 for now since control mixing in sim does not account for alignment)
+torque_coeff = 0.1; % Nm / % throttle
+
+% mav vars
+mav_inertia = eye(3);
+mav_mass = 0.6; %kg
