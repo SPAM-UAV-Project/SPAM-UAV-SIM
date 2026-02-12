be7a737861bf2a76258d6376ec7f659d343eb90c
Tommy Zhang <tommyz2005@gmail.com>
2026-02-12 03:49:43 -0500

    tune for spm2-6 13 inch prop

--- params.m
+++ params.m
@@ -1,24 +1,21 @@
-% scalar constants
-mass = 0.631; % kg
-
 % define body frame as FRD front right down
 % define inertial frame as NED north east down
 
 % blade angle model
-A_cut_in = 0.16; % modulation
-K_coeff = 19.95 * pi / 180; % rad / b_xy command
+A_cut_in = 0.01; % modulation
+K_coeff = 70 * pi / 180; % rad / b_xy command
 phase_lag = 0; % rad    (0 for now since control mixing in sim does not account for alignment)
 
 % prop model vars
-torque_coeff_top = 0.2051; % Nm / norm throttle: this is for the control allocator
+torque_coeff_top = 0.2551; % Nm / norm throttle: this is for the control allocator
 torque_coeff_bot = 0.1455;
-thrust_coeff_top = 15.9837; % N / norm throttle^2
+thrust_coeff_top = 15.3261; % N / norm throttle^2
 thrust_coeff_bot = 9.3460; % N / norm throttle^2
 
 % mav vars
 mav_inertia = eye(3) * 0.004013;
 mav_inertia(3, 3) = 0.0007873;
-mav_mass = 0.631; %kg
+mav_mass = 0.651; %kg
 top_motor_arm = [0, 0, 0.18]; % m from center of mass
 f_xy_tf = load("f_xy_tf.mat").tf_force_y;
 thrust_tf = load("thrust_tf.mat").thrust_tf;
@@ -29,8 +26,8 @@ rate_ctrl_time = 1/1000; % 1 ms
 
 mav_inertia_ctrl = eye(3) * 0.004013;
 mav_inertia_ctrl(3, 3) = 0.0007873;
-torque_coeff_thrust_top = 0.0187; % Nm / Force for control allocator
-torque_coeff_thrust_bot = 0.0155;
+torque_coeff_thrust_top = torque_coeff_top / thrust_coeff_top; % Nm / Force for control allocator
+torque_coeff_thrust_bot = torque_coeff_bot / thrust_coeff_bot;
 
 control_effectiveness_matrix = [
     0, 0, -1, -1;
@@ -57,7 +54,7 @@ control_allocator_matrix = inv(control_effectiveness_matrix)
 sample_time = 0.001; % s -> 1000 hz
 
 % initial conds
-initial_euler = [0, 0, 0];
+initial_euler = [0, 0.2, 0];
 
 
 %% test
